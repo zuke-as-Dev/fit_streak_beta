1dad236e2188200b0b37779f8733df4432516ab7
Zuke <rockbeat008@gmail.com>
2021-03-31 22:50:16 +0530

    loading screen done beta

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -12,23 +12,64 @@ class _loadingState extends State<loading> {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
+        child:Container(
+        decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('assets/bg.png'),
+              image: AssetImage('assets/cf.png'),
               fit: BoxFit.cover,
             )
           ),
-              child: Center(
-                child: SpinKitRing(
-                color: Colors.white,
-                size: 90.0,
-                lineWidth: 10.0,
-        ),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+              child: CircleAvatar(
+                radius: 90.0,
+                backgroundColor: Colors.transparent,
+                backgroundImage: AssetImage('assets/logo3.png'),
               ),
-      ),
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                child: Text(
+                  'Fit Streak',
+                  style: TextStyle(
+                    fontSize: 45.0,
+                    color: Colors.grey,
+                    fontFamily: 'conthrax',
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 60.0),
+            SpinKitRing(
+              color: Colors.white,
+              size: 110.0,
+              lineWidth: 10.0,
         ),
+          ],
+        ),
+    //     child: Center(
+    //       child: SpinKitRing(
+    //       color: Colors.white,
+    //       size: 90.0,
+    //       lineWidth: 10.0,
+    // ),
+    //     ),
 
+          // child: Container(
+          //   decoration: BoxDecoration(
+          //     image: DecorationImage(
+          //       image: AssetImage('assets/bh.jpg'),
+          //       fit: BoxFit.cover,
+          //     )
+          //   ),
+          //   ),
+        ),
+      ),
     );
   }
 }
