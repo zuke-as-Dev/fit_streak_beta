2c34ed24579b1d636a03065f3b0e5bf9204b4b57
Zuke <rockbeat008@gmail.com>
2021-03-31 13:58:32 +0530

    screens added

--- lib/loading.dart
+++ lib/loading.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
-//hello fuckin world
+
 void main() => runApp(MaterialApp(
   home: home()
 ));
-      // tanmay123
-      //Sunny
 class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "FitStreak Hey this is Godwin",
+          "Fit-streak",
               style: TextStyle(
             fontSize: 20.0,
         ),
